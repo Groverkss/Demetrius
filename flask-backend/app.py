07c3eb8869cbccabb7809401709847f029a1737f
Kunwar Shaanjeet Singh Grover <groverkss@gmail.com>
2020-11-08 20:39:15 +0530

    Added watson integration

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
 from pprint import pprint
 from ml.sarsa import find_amounts
+from watson import Watson
 
 app = Flask(__name__)
+chatbot = Watson()
 
 @app.route("/chat", methods=["POST"])
 def chat():
     req_data = request.get_json()
     input_query = req_data["input"]
-    
-    final_amounts = find_amounts()
-    pprint(final_amounts)
+   
+    response = chatbot.talk(input_query)
+    pprint(response)
 
     return "Testing"
 
---
+++ flask-backend/watson.py
@@ -0,0 +1,24 @@
+from ibm_watson import AssistantV2
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+
+
+class Watson:
+    def __init__(self):
+        self.api = os.environ["WATSON_API"]
+        self.url = os.environ["WATSON_URL"]
+        self.assistant_id = os.environ["WATSON_ID"]
+
+        self.authenticator = IAMAuthenticator(self.api)
+        self.assistant = AssistantV2(
+            version="2020-04-01", authenticator=self.authenticator
+        )
+        self.assistant.set_service_url(self.url)
+
+    def talk(self, text):
+        response = self.assistant.message_stateless(
+            assistant_id=self.assistant_id,
+            input={"message_type": "text", "text": text},
+        ).get_result()
+
+        return response
